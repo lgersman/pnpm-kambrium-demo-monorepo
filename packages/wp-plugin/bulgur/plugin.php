dbf28087eecbb16d2f117260dfde2e6f62679be6
Lars Gersmann <lars.gersmann@gmail.com>
2024-09-03 17:51:43 +0200

    chore(@pnpmkambrium-demo-monorepo/bulgur): removed obsolete metadata

--- packages/wp-plugin/bulgur/plugin.php
+++ packages/wp-plugin/bulgur/plugin.php
@@ -1,18 +1,17 @@
 <?php
 /**
- * Plugin Name: bulgur
- * Plugin URI: https://github.com/lgersman/pnpmkambrium
- * Description: Example block scaffolded with Create Block tool.
+ * Plugin Name:				bulgur
+ * Plugin URI: 	 			https://github.com/lgersman/pnpmkambrium
+ * Description:  			Example block scaffolded with Create Block tool.
  * Requires at least: 6.1
- * Requires PHP: 
- * Version: 0.1.0
- * Author: lars gersmann
- * License: MIT-2
- * License URI: https://opensource.org/licenses/MIT-2
- * Text Domain:       bulgur
- * Domain Path:       kambrium-bulgur
+ * Requires PHP:
+ * Version: 					0.1.0
+ * Author: 						lars gersmann
+ * License: 					MIT-2
+ * License URI: 			https://opensource.org/licenses/MIT-2
+ * Domain Path: 			/languages
  *
- * @package           kambrium-bulgur
+ * @package kambrium-bulgur
  */
 
 /**
@@ -22,7 +21,8 @@
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function kambrium_bulgur_bulgur_block_init() {
-	register_block_type( __DIR__ . '/build' );
+function kambrium_bulgur_bulgur_block_init()
+{
+	register_block_type(__DIR__ . '/build');
 }
-add_action( 'init', 'kambrium_bulgur_bulgur_block_init' );
+add_action('init', 'kambrium_bulgur_bulgur_block_init');
